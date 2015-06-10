84d34d6498589f1094ed5a0ee12cf5cb14b7405e
end3rs-army <eric.m.dowty@gmail.com>
2015-06-10 09:20:12 -0600

    the *path is any unknown pathe

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users
 
+  # The *path is a NOT FOUND route... basically redirects anything that is not a real route
   get '*path', :to => 'errors#show'
 
 end
